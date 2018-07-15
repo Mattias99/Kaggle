ea53e2f917e046967744d91545dbf1c4557a86f1
Mattias Karlsson <tillmattias@gmail.com>
2018-07-15 23:41:26 +0200

    New Kaggle competition

---
+++ Store_Item_Demand_Forecasting_Challenge/forecasting_challenge.R
@@ -0,0 +1,13 @@
+# Kaggle project
+# Store Item Demand Forecasting Challenge
+# Author: Mattias Karlsson
+# Date: 2018-07-15
+
+#### PACKAGE ####
+
+library("tidyverse")
+library("lintr")
+
+#### VARIABLES ####
+
+setwd("E:/Projekt/R-kod/Kaggle/Kaggle/Store_Item_Demand_Forecasting_Challenge")
