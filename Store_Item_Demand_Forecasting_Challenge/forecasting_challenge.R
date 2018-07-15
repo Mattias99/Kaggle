40a713a715c90803f6a813bada1d06a9de21dc77
Mattias Karlsson <tillmattias@gmail.com>
2018-07-16 00:20:11 +0200

    Added structure to Forecasting project

--- Store_Item_Demand_Forecasting_Challenge/forecasting_challenge.R
+++ Store_Item_Demand_Forecasting_Challenge/forecasting_challenge.R
@@ -5,9 +5,32 @@
 
 #### PACKAGE ####
 
+
 library("tidyverse")
 library("lintr")
 
+
 #### VARIABLES ####
 
+
 setwd("E:/Projekt/R-kod/Kaggle/Kaggle/Store_Item_Demand_Forecasting_Challenge")
+
+
+#### IMPORT ####
+
+
+train <- read_csv(file = "data/train.csv")
+test <- read_csv(file = "data/test.csv")
+
+
+##### DATA TRANSFORMATION ####
+
+example <- train %>%
+  group_by(store) %>%
+  summarise(numItems = n_distinct(item))
+example
+
+
+#### GGPLOT ####
+
+
