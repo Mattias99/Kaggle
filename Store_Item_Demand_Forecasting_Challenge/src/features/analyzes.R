01a4e13297c5fa68191bf5e472c0490e7d4213c3
Mattias99 <tillmattias@gmail.com>
2019-03-12 21:31:14 +0100

    Dickey-Fuller test and Table for non-stationarty/stationary series

--- Store_Item_Demand_Forecasting_Challenge/src/features/analyzes.R
+++ Store_Item_Demand_Forecasting_Challenge/src/features/analyzes.R
@@ -1,6 +1,6 @@
 # Analyzes
 
-#### Dickey-Fuller test ####
+#### Dickey Fuller test ####
 
 
 # Stationarity test
@@ -10,6 +10,30 @@ stationarity <- train_feature %>%
   group_by(store, item) %>%
   summarise(suppressWarnings(adf.test(sales)$p.value))
 
-colnames(stationarity)[3] <- "pValue"
+colnames(stationarity)[3] <- "p_value"
 
-nonStationary <- filter(stationarity, pValue > 0.05) %>% arrange(desc(pValue))
+non_stationary <- filter(stationarity, p_value > 0.05) %>%
+  arrange(desc(p_value))
+
+stationary_series <- filter(stationarity, p_value < 0.05) %>%
+  arrange(desc(p_value))
+
+
+addmargins(A = table(non_stationary[, c(1, 2)]))
+
+addmargins(A = table(stationary_series[, c(1, 2)]))
+
+####  Classic decomposistion ####
+
+
+# Convert serie to time-series object
+
+decompose_ts <- ts(train_decompose[, 4],
+                   frequency = 365,
+                   start = c(2013, 1, 1),
+                   end = c(2017, 12, 31))
+
+# Decompose function
+
+decompose_season <- decompose(decompose_ts,
+                              type = "additive")
